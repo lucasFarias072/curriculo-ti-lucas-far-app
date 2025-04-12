30584df2ecc186da0bf42851b35149c0682f72a3
Lucas-far <mizoguchia@hotmail.com>
2025-04-12 01:08:06 -0300

    general changes

--- templates/assets/js/script.js
+++ templates/assets/js/script.js
@@ -1,31 +1,43 @@
-function switchFrame(frameId) {
-  if (frameId === 1) {
-    cellphoneArea.appendChild(leftArea)
-    cellphoneArea.appendChild(middleArea)
-    cellphoneArea.appendChild(rightArea)
-    leftArea.style.width = '100%'
-    middleArea.style.width = '100%'
-    rightArea.style.width = '100%'
-  } else {
-    templateArea.className = 'flex row going-left'
-    templateArea.appendChild(leftArea)
-    templateArea.appendChild(middleArea)
-    templateArea.appendChild(rightArea)
-    leftArea.style.width = '25%'
-    middleArea.style.width = '37%'
-    rightArea.style.width = '37%'
-  }
+
+
+const backgrounds = {
+  light: 'linear-gradient(15deg, rgb(188, 188, 188), rgb(220, 220, 220), rgb(255, 255, 255))',
+  dark: 'linear-gradient(-15deg, black, rgb(0, 33, 33), rgb(0, 0, 33))'
 }
 
-const templateWindow = document.getElementById('window')
-const leftArea = document.getElementById('left-area')
-const middleArea = document.getElementById('middle-area')
-const rightArea = document.getElementById('right-area')
-const cellphoneArea = document.getElementById('cellphone-frame')
-const templateArea = document.getElementById('template-frame')
+const themes = {
+  light: '🌞',
+  dark: '🌜'
+}
 
-const loop = setInterval(() => {
-  const triggerPos = 850
-  // templateWindow.textContent = `[${window.innerHeight}] x [${window.innerWidth}]`
-  window.innerWidth <= triggerPos ? switchFrame(1) : switchFrame(2)
-}, 100)
+const themeArea = document.getElementById('theme-frame')
+const theme = document.getElementById('theme')
+const templateArea = document.getElementById('template-frame')
+const labels = document.querySelectorAll('.std-label')
+const projectBanners = document.querySelectorAll('.std-banner')
+
+theme.addEventListener('click', () => {
+  if (theme.textContent === themes.dark) {
+    theme.textContent = themes.light
+    templateArea.style.background = backgrounds.dark
+    themeArea.style.background = backgrounds.dark
+    templateArea.style.color = 'darkgray'
+    templateArea.style.textShadow = '0 0 .2rem #222'
+    labels.forEach(labelTag => labelTag.classList.add('std-label-dark'))
+    projectBanners.forEach(bannerTag => {
+      bannerTag.style.boxShadow = '0 0 1rem rgb(0, 99, 99)'
+      bannerTag.style.border = 'rgb(0, 99, 99) solid 1px'
+    })
+  } else {
+    theme.textContent = themes.dark
+    templateArea.style.background = backgrounds.light
+    themeArea.style.background = backgrounds.light
+    templateArea.style.color = 'inherit'
+    templateArea.style.textShadow = 'inherit'
+    labels.forEach(labelTag => labelTag.classList.remove('std-label-dark'))
+    projectBanners.forEach(bannerTag => {
+      bannerTag.style.boxShadow = '0 0 1rem rgb(128, 128, 128)'
+      bannerTag.style.border = 'rgb(128, 128, 128) solid 1px'
+    })
+  }
+})
